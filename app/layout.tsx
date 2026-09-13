e6eaaf0c61ef9d20360d2b0feb8cb971c329ad8a
asepjamaludinn <asepjamaludinn24@gmail.com>
2026-09-13 14:27:21 +0700

    feat: add SEO favicons and manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Montserrat, Poppins } from "next/font/google";
 import "./globals.css";
 import { QueryProvider } from "@/providers/query-provider";
@@ -13,6 +14,32 @@ const poppins = Poppins({
   variable: "--font-secondary",
   weight: ["400", "500", "600"],
 });
+
+export const metadata: Metadata = {
+  title: "MBC Praktikum | Laboratorium Manajemen Telkom University",
+  description:
+    "Portal resmi sistem informasi dan manajemen praktikum MBC Laboratory Telkom University.",
+  keywords: [
+    "mbc praktikum",
+    "mbc laboratory",
+    "telkom university",
+    "praktikum mbc",
+    "asisten mbc",
+  ],
+
+  appleWebApp: {
+    title: "MBC Praktikum",
+  },
+  openGraph: {
+    title: "MBC Praktikum Portal",
+    description:
+      "Portal resmi manajemen praktikum MBC Laboratory Telkom University.",
+    url: "https://mbcpraktikum.com",
+    siteName: "MBC Praktikum",
+    locale: "id_ID",
+    type: "website",
+  },
+};
 
 export default function RootLayout({
   children,
